746f04ce18d3472c3dcf3e07ca5d08066c39609b
Raszero <travisjcelli95@gmail.com>
2023-12-25 10:23:43 -0600

    set User model

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class User extends Model {}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isAlphanumeric: true,
+    },
+  },
+});
