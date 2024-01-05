4b6bb89920775e55c872a2390346db9ecb79a65a
Raszero <travisjcelli95@gmail.com>
2024-01-04 19:01:33 -0600

    added relationship

--- models/index.js
+++ models/index.js
@@ -6,9 +6,17 @@ User.hasOne(TimeCard, {
   foreignKey: "user_id",
 });
 
+TimeCard.belongsTo(User, {
+  foreignKey: "user_id"
+})
+
 TimeCard.hasMany(TimeEvent, {
   foreignKey: "timeEvent_id",
 });
+
+// TimeCard.hasMany(TimeEvent, {
+//   foreignKey: "user_id"
+// })
 
 module.exports = {
   User,
