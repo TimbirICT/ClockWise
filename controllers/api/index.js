625b6a14285f4324ac8e8ec410bbb8f289d5885a
Raszero <travisjcelli95@gmail.com>
2024-01-09 00:41:17 -0600

    fixed login bug, added comments to main handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,12 +3,10 @@ const userRoutes = require("./UserRoutes");
 const timeCardRoutes = require("./TimeCardRoutes");
 const timeEventRoutes = require("./TimeEventRoutes");
 const homeRoutes = require("../homeRoutes");
-const portalRoutes = require("../portalRoutes")
 
 router.use("/users", userRoutes);
 router.use("/timecards", timeCardRoutes);
 router.use("/timeevents", timeEventRoutes);
 router.use('/', homeRoutes);
-router.use("/portal", portalRoutes)
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -3,18 +3,18 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
     // get values of name and password input
-    const email = document.querySelector("#email-login").value.trim();
-    const password = document.querySelector("#password-login").value.trim();
+    const email = document.getElementById("email-login").value.trim();
+    const password = document.getElementById("password-login").value.trim();
   
-    if (username && password) {
-      const response = await fetch("api/users/login", {
+    if (email && password) {
+      const response = await fetch("/api/users/login", {
         method: "POST",
         body: JSON.stringify({ email, password }),
         headers: { "Content-Type": "application/json" },
       });
   
       if (response.ok) {
-        document.location.replace("/");
+        document.location.replace("/portal");
       } else {
         alert("Failed to log in.");
       }
