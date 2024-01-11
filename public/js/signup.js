f1c7ae7c4c2d29186cd145fefbdb54ebe7a28380
Raszero <travisjcelli95@gmail.com>
2024-01-10 18:25:37 -0600

    create signup route and function

---
+++ public/js/signup.js
@@ -0,0 +1,26 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.getElementById("name-signup").value.trim();
+    const email = document.getElementById("email-signup").value.trim();
+    const password = document.getElementById("password-signup").value.trim();
+  
+    if (name && email && password) {
+      const response = await fetch("/api/users/signup", {
+        method: "POST",
+        body: JSON.stringify({ name, email, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+      console.log(name, email, password);
+  
+      if (response.ok) {
+        document.location.replace("/");
+      } else {
+        alert("Failed to sign up.");
+      }
+    }
+  };
+  
+  document
+    .getElementById("signup-form")
+    .addEventListener("submit", signupFormHandler);
